01283521922b6e52696f5f3196ee240a6b8b133d
Pastilhex <ivodanielmarques@gmail.com>
2023-08-09 06:07:27 +0100

    gitignore setup

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 10:57:05 by ialves-m          #+#    #+#             */
-/*   Updated: 2023/08/09 06:03:58 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/08/09 06:07:22 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int	main(int argc, char **argv)
 	// m.time_to_eat = ft_atoi(argv[c.i++]);
 	// m.time_to_sleep = ft_atoi(argv[c.i++]);
 	// m.number_of_times_each_philosopher_must_eat = ft_atoi(argv[c.i]);
+	
 	// philo = (pthread_t *)malloc(m.number_of_philosophers * sizeof(pthread_t));
 	
 	pthread_mutex_init(&mutex, NULL);
